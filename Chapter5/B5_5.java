5cbad6079225ccfa8b6c20fb568ba491cbaadba9
BK-0717 <72550527+CanisMinor-1037@users.noreply.github.com>
2022-04-10 18:54:43 +0800

    Add files via upload

---
+++ Chapter5/B5_5.java
@@ -0,0 +1,9 @@
+public class B5_5 {
+    public static void main(String[] args) {
+        int x = 80000000;
+        while(x>0)
+            x++;
+
+        System.out.println("x is " + x);
+    }
+}
