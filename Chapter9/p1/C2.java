b666d512529b377523df9c85f312a430f3e5e7c5
BK-0717 <72550527+CanisMinor-1037@users.noreply.github.com>
2022-04-10 19:01:20 +0800

    Add files via upload

---
+++ Chapter9/p1/C2.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class C2 {
+    void aMethod(){
+        C1 o = new C1();
+        //o.x;
+        //o.y;
+        //o.z;
+        //o.m1()
+        //o.m2()
+        //o.m3()
+    }    
+}
