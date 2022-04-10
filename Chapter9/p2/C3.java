7bdd1c3bb0a48f4833d0d6d027f5fa64232c0c69
BK-0717 <72550527+CanisMinor-1037@users.noreply.github.com>
2022-04-10 19:01:53 +0800

    Create C3.java

---
+++ Chapter9/p2/C3.java
@@ -0,0 +1,15 @@
+package p2;
+
+import p1.C1;
+
+public class C3 {
+    void aMethod(){
+        C1 o = new C1();
+        //C1.x
+        //C1.y
+        //C1.z
+        //C1.m1()
+        //C1.m2()
+        //C1.m3()
+    }
+}
